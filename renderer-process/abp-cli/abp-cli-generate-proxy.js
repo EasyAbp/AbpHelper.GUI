eab78c39e5919cc3c9df82fa8ea495b673826e62
gdlcf88 <47396430@qq.com>
2020-05-28 22:14:20 +0800

    Fixed generate-proxy usage

--- renderer-process/abp-cli/abp-cli-generate-proxy.js
+++ renderer-process/abp-cli/abp-cli-generate-proxy.js
@@ -1,3 +1,4 @@
+const { dialog } = require('electron').remote
 const exec = require('child_process').exec
 
 let isRunning = false
@@ -5,17 +6,30 @@ let isRunning = false
 let consoleNode = document.getElementById('box-abp-cli-generate-proxy').getElementsByTagName('textarea')[0]
 
 const execBtn = document.getElementById('generate-proxy-execute')
-
+const projectSelectBtn = document.getElementById('generate-proxy-project-selectBtn')
 
 execBtn.addEventListener('click', (event) => {
   runExec()
 })
 
+projectSelectBtn.addEventListener('click', (event) => {
+  dialog.showOpenDialog({
+    properties: ['openDirectory']
+  }).then(result => {
+    if (result.filePaths[0]) {
+      document.getElementById('generate-proxy-project').value = result.filePaths[0]
+    }
+  }).catch(err => {
+    console.log(err)
+  })
+})
+
 function runExec() {
+  let cmdPath = document.getElementById('generate-proxy-project').value
   let apiUrl = document.getElementById('generate-proxy-api-url').value
   let ui = document.getElementById('generate-proxy-ui').value
   let module = document.getElementById('generate-proxy-module').value
-  if (isRunning || !apiUrl) return
+  if (isRunning || !cmdPath) return
   isRunning = true
   execBtn.disabled = true
   document.getElementById('generate-proxy-process').style.display = 'block'
@@ -29,7 +43,7 @@ function runExec() {
   scrollConsoleToBottom()
   console.log(cmdStr)
   if (process.platform === 'win32') cmdStr = '@chcp 65001 >nul & cmd /d/s/c ' + cmdStr
-  workerProcess = exec(cmdStr, {cwd: '/'})
+  workerProcess = exec(cmdStr, {cwd: cmdPath})
   
   workerProcess.stdout.on('data', function (data) {
     addConsoleContent(data)
