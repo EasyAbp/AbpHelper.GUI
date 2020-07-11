1fde341597e64c69a1e93ff9cf7bf0a23573962b
gdlcf88 <gdlcf88@outlook.com>
2020-07-12 02:23:25 +0800

    Implement localization generation feature

---
+++ renderer-process/abphelper-cli/abphelper-cli-generate-localization-item.js
@@ -0,0 +1,107 @@
+const { dialog } = require('electron').remote
+const exec = require('child_process').exec
+const fs = require('fs')
+
+let isRunning = false
+
+let extraOptions = {
+}
+
+let consoleNode = document.getElementById('box-abphelper-cli-generate-localization-item').getElementsByTagName('textarea')[0]
+
+const execBtn = document.getElementById('localization-item-execute')
+const selectSolutionFileBtn = document.getElementById('localization-item-select-solution-file-btn')
+const extraOptionsCheckBox = {
+}
+
+selectSolutionFileBtn.addEventListener('click', (event) => {
+  dialog.showOpenDialog({
+    filters: [
+      { name: 'Abp Solution', extensions: ['sln'] },
+    ],
+    properties: ['openFile']
+  }).then(result => {
+    if (result.filePaths[0]) {
+      document.getElementById('localization-item-solution-file').value = result.filePaths[0]
+    }
+  }).catch(err => {
+    console.log(err)
+  })
+})
+
+function findLastStr(str, cha, num) {
+  let times = num == 0 ? 1 : num;
+  var x = str.lastIndexOf(cha);
+  for (var i = 0; i < times - 1; i++) {
+      x = str.lastIndexOf(cha, x - 1);
+  }
+  return x;
+}
+
+function getSolutionRootPath(slnFilePath) {
+  let separator = slnFilePath.indexOf('/') != -1 ? '/' : '\\'
+  let strs = slnFilePath.split(separator)
+  if (strs.length > 1 && strs[strs.length - 2] === 'aspnet-core') {
+    // is app
+    return slnFilePath.substr(0, findLastStr(slnFilePath, separator, 2))
+  }
+  var moduleRootPath = slnFilePath.substr(0, findLastStr(slnFilePath, separator, 1))
+  if (fs.existsSync(moduleRootPath + separator + 'host')) {
+    // is module
+    return moduleRootPath
+  }
+  return alert('App solution\'s .sln file should be in the "aspnet-core" folder. Module solution should have "host" folder.')
+}
+
+execBtn.addEventListener('click', (event) => {
+  runExec()
+})
+
+function runExec() {
+  let names = document.getElementById('localization-item-names').value
+  let solutionFile = document.getElementById('localization-item-solution-file').value
+  if (isRunning || !names || !solutionFile) return
+  
+  let solutionRootPath = getSolutionRootPath(solutionFile)
+  if (!solutionRootPath) return
+
+  isRunning = true
+  execBtn.disabled = true
+  document.getElementById('localization-item-process').style.display = 'block'
+
+  let cliCommand = process.platform === 'win32' ? '%USERPROFILE%\\.dotnet\\tools\\abphelper' : '$HOME/.dotnet/tools/abphelper'
+  let cmdStr = cliCommand + ' generate localization ' + names + ' -d ' + solutionRootPath
+  clearConsoleContent()
+  addConsoleContent(cmdStr + '\n\nRunning...\n')
+  scrollConsoleToBottom()
+  console.log(cmdStr)
+  if (process.platform === 'win32') cmdStr = '@chcp 65001 >nul & cmd /d/s/c ' + cmdStr
+  workerProcess = exec(cmdStr, {cwd: '/'})
+  
+  workerProcess.stdout.on('data', function (data) {
+    addConsoleContent(data)
+    scrollConsoleToBottom()
+  });
+ 
+  workerProcess.stderr.on('data', function (data) {
+    addConsoleContent(data)
+    scrollConsoleToBottom()
+  });
+ 
+  workerProcess.on('close', function (code) {
+    isRunning = false
+    execBtn.disabled = false
+  })
+
+  function scrollConsoleToBottom() {
+    consoleNode.scrollTo(0, consoleNode.scrollHeight)
+  }
+
+  function addConsoleContent(text) {
+    consoleNode.appendChild(document.createTextNode(text))
+  }
+
+  function clearConsoleContent() {
+    consoleNode.innerHTML = ''
+  }
+}
