01439dc320cea5916ea6b648a8b42a1ad7e97fc9
gdlcf88 <gdlcf88@outlook.com>
2021-02-21 01:43:36 +0800

    Improve the home page.

--- dotnet/src/EasyAbp.AbpHelper.Gui.Blazor/GuiBrandingProvider.cs
+++ dotnet/src/EasyAbp.AbpHelper.Gui.Blazor/GuiBrandingProvider.cs
@@ -6,6 +6,8 @@ namespace EasyAbp.AbpHelper.Gui.Blazor
     [Dependency(ReplaceServices = true)]
     public class GuiBrandingProvider : DefaultBrandingProvider
     {
-        public override string AppName => "AbpHelper";
+        public override string AppName => "";
+        public override string LogoUrl => "/images/app-icon/icon.svg";
+        public override string LogoReverseUrl => "/images/app-icon/icon.svg";
     }
 }
