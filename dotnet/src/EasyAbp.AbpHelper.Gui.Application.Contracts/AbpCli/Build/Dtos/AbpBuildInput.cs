bd800f5f716b95de8eb1256e84da3b093531be53
gdlcf88 <gdlcf88@outlook.com>
2021-02-25 20:35:41 +0800

    ABP CLI: build.

---
+++ dotnet/src/EasyAbp.AbpHelper.Gui.Application.Contracts/AbpCli/Build/Dtos/AbpBuildInput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EasyAbp.AbpHelper.Gui.Shared.Dtos;
+using JetBrains.Annotations;
+
+namespace EasyAbp.AbpHelper.Gui.AbpCli.Build.Dtos
+{
+    [Serializable]
+    public class AbpBuildInput : InputDtoWithRunningPath
+    {
+        [CanBeNull]
+        public virtual string BuildName { get; set; }
+        
+        [CanBeNull]
+        public virtual string DotnetBuildArguments { get; set; }
+        
+        public virtual bool Force { get; set; }
+
+        public AbpBuildInput()
+        {
+        }
+
+        protected AbpBuildInput([NotNull] string runningPath, [CanBeNull] string buildName,
+            [CanBeNull] string dotnetBuildArguments, bool force) : base(runningPath)
+        {
+            BuildName = buildName;
+            DotnetBuildArguments = dotnetBuildArguments;
+            Force = force;
+        }
+    }
+}
---
+++ dotnet/src/EasyAbp.AbpHelper.Gui.Blazor/Pages/AbpCli/Components/Build/Build.razor.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using EasyAbp.AbpHelper.Gui.AbpCli.Build;
+using Microsoft.AspNetCore.Components;
+
+namespace EasyAbp.AbpHelper.Gui.Blazor.Pages.AbpCli.Components.Build
+{
+    public partial class Build
+    {
+        [Inject]
+        private IAbpCliBuildAppService Service { get; set; }
+
+        protected override async Task InternalExecuteAsync()
+        {
+            await Service.BuildAsync(Input);
+        }
+    }
+}
