6c218be919c29464a8850a9cad8d490c277e7f76
gdlcf88 <gdlcf88@outlook.com>
2021-02-28 03:07:15 +0800

    Change the timeout value of dynamic proxy http clients to 10 minutes.

---
+++ dotnet/src/EasyAbp.AbpHelper.Gui.Blazor/DynamicProxyHttpClientFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Http.Client.DynamicProxying;
+
+namespace EasyAbp.AbpHelper.Gui.Blazor
+{
+    [Dependency(ReplaceServices = true)]
+    public class DynamicProxyHttpClientFactory : IDynamicProxyHttpClientFactory, ITransientDependency
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DynamicProxyHttpClientFactory(IHttpClientFactory httpClientFactory) 
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        
+        public HttpClient Create()
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            client.Timeout = TimeSpan.FromMinutes(10);
+            
+            return client;
+        }
+
+        public HttpClient Create(string name)
+        {
+            var client = _httpClientFactory.CreateClient(name);
+            
+            client.Timeout = TimeSpan.FromMinutes(10);
+
+            return client;
+        }
+    }
+}
