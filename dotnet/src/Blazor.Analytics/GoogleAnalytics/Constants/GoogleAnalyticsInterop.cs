df0f28a8591de87fd07a627cfc945b1a580c5c5b
gdlcf88 <gdlcf88@outlook.com>
2021-05-11 19:00:45 +0800

    Use Blazorx-Analytics

--- dotnet/src/Blazor.Analytics/GoogleAnalytics/Constants/GoogleAnalyticsInterop.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Blazorx.Analytics.Constants
-{
-    internal static class GoogleAnalyticsInterop
-    {
-        public const string Configure = "GoogleAnalyticsInterop.configure";
-        public const string Navigate = "GoogleAnalyticsInterop.navigate";
-        public const string TrackEvent = "GoogleAnalyticsInterop.trackEvent";
-    }
-}
