0fa6a1bb23263505f8a05d68678d832b53c4d885
gdlcf88 <gdlcf88@outlook.com>
2022-05-14 03:43:44 +0800

    Use LeptonX Lite theme

--- dotnet/src/EasyAbp.AbpHelper.Gui.Blazor/GuiBrandingProvider.cs
+++ dotnet/src/EasyAbp.AbpHelper.Gui.Blazor/GuiBrandingProvider.cs
@@ -6,7 +6,7 @@ namespace EasyAbp.AbpHelper.Gui.Blazor
     [Dependency(ReplaceServices = true)]
     public class GuiBrandingProvider : DefaultBrandingProvider
     {
-        public override string AppName => "";
+        public override string AppName => "1234";
         public override string LogoUrl => "/images/app-icon/icon.png";
         public override string LogoReverseUrl => "/images/app-icon/icon.png";
     }
--- dotnet/src/EasyAbp.AbpHelper.Gui.Blazor/Toolbars/GuiToolbarContributor.cs
+++ dotnet/src/EasyAbp.AbpHelper.Gui.Blazor/Toolbars/GuiToolbarContributor.cs
@@ -1,7 +1,7 @@
 ﻿using System.Threading.Tasks;
 using EasyAbp.AbpHelper.Gui.Blazor.Pages.LogService.Components;
 using EasyAbp.AbpHelper.Gui.Blazor.Pages.Solutions.Components;
-using Volo.Abp.AspNetCore.Components.Server.BasicTheme.Themes.Basic;
+using Volo.Abp.AspNetCore.Components.Server.LeptonXLiteTheme.Themes.LeptonXLite.Toolbar;
 using Volo.Abp.AspNetCore.Components.Web.Theming.Toolbars;
 
 namespace EasyAbp.AbpHelper.Gui.Blazor.Toolbars
@@ -16,7 +16,7 @@ namespace EasyAbp.AbpHelper.Gui.Blazor.Toolbars
             }
             
             // Remove the login item.
-            context.Toolbar.Items.RemoveAll(x => x.ComponentType == typeof(LoginDisplay));
+            context.Toolbar.Items.RemoveAll(x => x.ComponentType == typeof(UserMenuComponent));
             
             context.Toolbar.Items.Insert(0, new ToolbarItem(typeof(Logs)));
             
