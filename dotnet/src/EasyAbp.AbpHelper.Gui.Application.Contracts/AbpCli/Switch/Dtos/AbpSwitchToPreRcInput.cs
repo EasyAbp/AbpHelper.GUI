3755ef4105c08a9776c6d1ad4aae9c7346f7b158
gdlcf88 <gdlcf88@outlook.com>
2024-05-19 18:52:39 +0800

    Upgrade to ABP 8.1.3

---
+++ dotnet/src/EasyAbp.AbpHelper.Gui.Application.Contracts/AbpCli/Switch/Dtos/AbpSwitchToPreRcInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using EasyAbp.AbpHelper.Gui.Shared.Dtos;
+using JetBrains.Annotations;
+
+namespace EasyAbp.AbpHelper.Gui.AbpCli.Switch.Dtos
+{
+    [Serializable]
+    public class AbpSwitchToPreRcInput : InputDtoWithDirectory
+    {
+        public AbpSwitchToPreRcInput()
+        {
+        }
+
+        public AbpSwitchToPreRcInput([NotNull] string directory) : base(directory)
+        {
+        }
+    }
+}
---
+++ dotnet/src/EasyAbp.AbpHelper.Gui.Blazor/Pages/AbpCli/Components/Switch/SwitchToPreRc.razor.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using EasyAbp.AbpHelper.Gui.AbpCli.Switch;
+using Microsoft.AspNetCore.Components;
+
+namespace EasyAbp.AbpHelper.Gui.Blazor.Pages.AbpCli.Components.Switch
+{
+    public partial class SwitchToPreRc
+    {
+        [Inject]
+        private IAbpCliSwitchAppService Service { get; set; }
+
+        protected override async Task InternalExecuteAsync()
+        {
+            await Service.SwitchToPreRcAsync(Input);
+        }
+    }
+}
